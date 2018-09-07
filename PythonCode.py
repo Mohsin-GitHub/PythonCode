c9871c206efa86f3f02bcfefa859d926489c02db
Mohsin-Github <soft.mohsin@gmail.com>
2018-09-08 01:21:02 +0800

    practise

--- PythonCode.py
+++ PythonCode.py
@@ -1,10 +1,24 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
+import sys as sy
 
 msg="Hell World. We rock"
 print(msg)
 
-x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
-plt.plot(x, np.sin(x))       # Plot the sine of each x point
-plt.show()                   # Display the plot
+#x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
+#plt.plot(x, np.sin(x))       # Plot the sine of each x point
+#plt.show()                   # Display the plot
+
+flag=True
+if(flag):
+    print "Success"
+
+def fabonacci(n=10):
+    a,b=0,1
+    while a<n:
+        print(a)
+        a,b=b,a+b
+    print()
+
+fabonacci(100)
